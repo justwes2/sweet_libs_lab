23278041af0b28679aeb79eecab37529773cbfd6
Wes <wes.coffay@gmail.com>
2017-03-23 16:32:42 -0400

    index.html created and will populate correctly

--- sweetlibs/polls/urls.py
+++ sweetlibs/polls/urls.py
@@ -1,9 +1,16 @@
-# This is where you will map the view you just defined to a url. 
+# This is where you will map the view you just defined to a url.
 from django.conf.urls import url
 # This line pulls url paths from the root url file. More on this next
 from . import views
 # This is where the views are called from this file, rather than the parent file.
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # ex /polls/
+    url(r'^$', views.index, name='index'),
+    # ex /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$',views.detail, name='detail'),
+    # ex /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
 # This is where the routes are defined.
--- sweetlibs/polls/views.py
+++ sweetlibs/polls/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import HttpResponse
 # make sure to change the above to HttpResponse, it is not the default, and not doing so will prevent the view from rendering
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
-# this simple view will render the text above at the defined path. 
+    # this simple view will render the text above at the defined path.
+    # return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
